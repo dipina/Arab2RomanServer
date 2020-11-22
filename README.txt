693f722e8d344da54024846430dff54f41d5b51b
dipina <dipina@gmail.com>
2020-11-22 10:39:26 +0100

    first upload of Arab2RomanConverter front-end as PWA

---
+++ README.txt
@@ -0,0 +1,9 @@
+1. Documentation of PhoneGap is available at http://docs.phonegap.com/phonegap-build/getting-started/
+2. Notice that you have to edit file index.html variable PREFIX_URL to point to the location of the back-end in the cloud
+3. Notice how the original app in node.js has been factorized to separate cleanly the client and server side
+4. Go to: https://build.phonegap.com
+5. Sign in and/or log in
+6. Click on Apps
+7. Select Update Code and upload there a .zip file with the contents of this folder
+8. Download the APK generated
+9. Transfer it through Bluetooth to your device
---
+++ docs/sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'Arab2RomanConverter-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/css/style.css',
+  '/css/my.css',
+  '/css/jquery.mobile-1.1.1.min.css',
+  '/js/jquery.min.js',
+  '/js/jquery.mobile-1.1.1.min.js',
+  '/js/my.js',
+  '/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
